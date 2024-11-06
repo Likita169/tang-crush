8296de8f861176b7c347a2b9cd0fd53e071c8d43
Likita169 <157664973+Likita169@users.noreply.github.com>
2024-11-06 20:05:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Hee luu cậu!',
     introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'oke',
+    btnIntro: 'oke cậu',
     title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là người yêu tớ đó nha :> ',
     btnYes: 'yêu ơi là iuuu <33',
     btnNo: 'cậu mơ à :333',
-    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
+    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3(bấm bất kì phím nào nó sẽ dần hiện ra thông điệp ẩn)',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
+    reply: 'Mình biết là cậu miễn cưỡng ấn, nhưng mà cậu có thể cho mình một cơ hội để yêu cậu được không nếu cậu chưa biết gì về mình thì mình có thể tìm hiểu nhau nà^^',
     mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/Huylikita' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
